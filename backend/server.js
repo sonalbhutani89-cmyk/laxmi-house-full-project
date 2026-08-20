20d7072d3c7f25b945581fb93ef9a0f3394ddd58
sonal <sonalbhutani89@gmail.com>
2026-08-20 14:35:52 +0530

    update code

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,23 @@
 const path = require("path");
 require("dotenv").config();
 const express = require("express");
-
 const connectDB = require("./config/db");
-
-
-
 
 const app = express();
 
 app.use(express.json());
 
-// app.use("/api", require("./routes/contactRoutes"));
+app.use("/api", require("./routes/contactRoutes"));
 
-// app.get("/", (req, res) => {
-//   res.send("Backend Running");
-// });
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get('/*splat', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-// });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,async () => {
+app.listen(PORT, async () => {
   console.log(`Server Running on ${PORT}`);
-  
   await connectDB();
 });
