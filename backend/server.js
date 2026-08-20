5154a385453adc2d9a4f26a3090e93e08335a259
sonal <sonalbhutani89@gmail.com>
2026-08-20 14:06:53 +0530

    update code

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,13 @@ app.use(express.json());
 // });
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+app.get('/*splat', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+// });
 
 const PORT = process.env.PORT || 5000;
 
